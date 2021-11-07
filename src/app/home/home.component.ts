54daca2e7ad55c423d543bce809a2594581fb715
Rodoba96 <A01702398@itesm.mx>
2021-11-06 18:30:22 -0600

    add users #1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  user: string = ""
+  pass: string = ""
+  token: string = ""
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  loginUser() {
+
+    alert(this.user + " - " + this.pass);
+    /*this.graphqlUsersService.tokenAuth(this.user, this.pass)
+    .subscribe(({ data }) => {
+       console.log('logged: ', JSON.stringify(data));
+      this.token =  JSON.parse(JSON.stringify(data)).tokenAuth.token;
+
+    }, (error) => {
+       console.log('there was an error sending the query', error);
+    });*/
+  
+  } 
 
 }
